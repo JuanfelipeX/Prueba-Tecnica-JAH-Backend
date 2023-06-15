0d80e9b291ab8db5a37333acc5c92f5b53c91aaf
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2023-06-15 11:05:43 -0500

    terminando de hacer los demas endpoits

--- routes/user.js
+++ routes/user.js
@@ -27,6 +27,65 @@ router.post('/', (req, res) => {
     });
 });
 
-// Resto de los endpoints y lógica de la API
+// Obtener los detalles de un usuario específico
+router.get('/:id', (req, res) => {
+  const userId = req.params.id;
+
+  User.findByPk(userId)
+    .then((user) => {
+      if (user) {
+        res.json(user);
+      } else {
+        res.status(404).json({ error: 'Usuario no encontrado' });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'Error al obtener los detalles del usuario' });
+    });
+});
+
+// Actualizar los detalles de un usuario específico
+router.put('/:id', (req, res) => {
+  const userId = req.params.id;
+  const { name, email, image } = req.body;
+
+  User.findByPk(userId)
+    .then((user) => {
+      if (user) {
+        user.name = name;
+        user.email = email;
+        user.image = image;
+        return user.save();
+      } else {
+        res.status(404).json({ error: 'Usuario no encontrado' });
+      }
+    })
+    .then((updatedUser) => {
+      res.json(updatedUser);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'Error al actualizar los detalles del usuario' });
+    });
+});
+
+// Eliminar un usuario específico
+router.delete('/:id', (req, res) => {
+  const userId = req.params.id;
+
+  User.findByPk(userId)
+    .then((user) => {
+      if (user) {
+        return user.destroy();
+      } else {
+        res.status(404).json({ error: 'Usuario no encontrado' });
+      }
+    })
+    .then(() => {
+      res.json({ message: 'Usuario eliminado correctamente' });
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'Error al eliminar el usuario' });
+    });
+});
 
 module.exports = router;
